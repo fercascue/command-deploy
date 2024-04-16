cdd41ab4c8bd5e840574f7d10149aa45d873b7d4
fercascue <fercascue@gmail.com>
2024-04-16 20:09:15 +0200

    feat: update app1

--- app1/src/main/java/software/amazonaws/example/product/entrypoints/ApiGatewayDeleteProductRequestHandler.java
+++ app1/src/main/java/software/amazonaws/example/product/entrypoints/ApiGatewayDeleteProductRequestHandler.java
@@ -21,7 +21,7 @@ public class ApiGatewayDeleteProductRequestHandler implements RequestHandler<API
     private static final Logger logger = LoggerFactory.getLogger(ApiGatewayDeleteProductRequestHandler.class);
     private final ProductStore productStore = new DynamoDbProductStore();
 
-    //REVIEW - 10
+    //REVIEW - 11
     @Override
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
         String id = event.getPathParameters().get("id");
